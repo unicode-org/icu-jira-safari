a131c4ed6a8d5867449e4100256da3822b5a7859
Steven R. Loomis <srloomis@us.ibm.com>
2018-07-25 12:59:11 -0700

    linkify PRs

--- IcuSafariJira.safariextension/icu-safari-jira.js
+++ IcuSafariJira.safariextension/icu-safari-jira.js
@@ -3,30 +3,37 @@
 // License & terms of use: http://www.unicode.org/copyright.html
 
 'use strict';
-const ces = document.getElementsByClassName('commit');
 
-for(let ce of ces) {
-    const linksBox = document.createElement('div');
-    linksBox.appendChild(document.createTextNode('JIRA: '));
-    ce.appendChild(linksBox);
+function icuLinkify(clazz) {
+    const ces = document.getElementsByClassName(clazz);
+    for(let ce of ces) {
+        const linksBox = document.createElement('div');
 
-    // now, what to add?
-    const s = new Set(); // set of issue links
+        // now, what to add?
+        const s = new Set(); // set of issue links
 
-    for(let subnode of ce.childNodes) {
-        const linkmatch=/ICU-[0-9]+/g;
-        let r;
-        while ((r = linkmatch.exec(subnode.textContent)) !== null) {
-            s.add(r[0]);
+        for(let subnode of ce.childNodes) {
+            const linkmatch=/ICU-[0-9]+/g;
+            let r;
+            while ((r = linkmatch.exec(subnode.textContent)) !== null) {
+                s.add(r[0]);
+            }
+        }
+
+        if(s.size) {
+            linksBox.appendChild(document.createTextNode('JIRA: '));
+            ce.appendChild(linksBox);
+        
+            for(let v of s.values()) {
+                const a = document.createElement('a');
+                a.appendChild(document.createTextNode(v));
+                a.href=`https://unicode-org.atlassian.net/browse/${v}`;
+                linksBox.appendChild(a);
+                linksBox.appendChild(document.createTextNode(' '));
+            }
         }
     }
-
-    for(let v of s.values()) {
-        const a = document.createElement('a');
-        a.appendChild(document.createTextNode(v));
-        a.href=`https://unicode-org.atlassian.net/browse/${v}`;
-        linksBox.appendChild(a);
-        linksBox.appendChild(document.createTextNode(' '));
-    }
 }
-
+icuLinkify('commit');
+icuLinkify('gh-header-title');
+icuLinkify('js-issue-row');

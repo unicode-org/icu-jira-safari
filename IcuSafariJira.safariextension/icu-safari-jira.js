688b5c091d5c68ed0d565a1667e46c6207b6ed2f
Steven R. Loomis <srloomis@us.ibm.com>
2018-07-25 14:30:18 -0700

    add optional stuff if CORS is disabled

--- IcuSafariJira.safariextension/icu-safari-jira.js
+++ IcuSafariJira.safariextension/icu-safari-jira.js
@@ -4,36 +4,69 @@
 
 'use strict';
 
-function icuLinkify(clazz) {
-    const ces = document.getElementsByClassName(clazz);
-    for(let ce of ces) {
-        const linksBox = document.createElement('div');
+// TODO: should be onload
+(function (){
+    const jirasite = 'https://unicode-org.atlassian.net';
+    const proxsite = jirasite;
+    // const _icuLinkifyCache = {};
 
-        // now, what to add?
-        const s = new Set(); // set of issue links
+    // WILL ONLY WORK if you disable CORS somehow.
+    function updateStatus(id, elem) {
+        if(window.fetch) {
+            fetch(`${proxsite}/rest/api/2/issue/${id}?fields=status`)
+            .then(function(response) {
+                const json = response.json();
+                json.then((json) => {
+                    if(json && json.fields && json.fields.status) {
+                        const aspan = document.createElement('span');
+                        aspan.className='IssueLabel text-small';
+                        aspan.appendChild(document.createTextNode(json.fields.status.name));
+                        elem.appendChild(document.createTextNode(' '));
+                        elem.appendChild(aspan);
+                        elem.prep
+                    }
+                });
+            })
+            .then(function(myJson) {
+                // console.log(myJson);
+            });
+        }      
+    }
 
-        for(let subnode of ce.childNodes) {
-            const linkmatch=/ICU-[0-9]+/g;
-            let r;
-            while ((r = linkmatch.exec(subnode.textContent)) !== null) {
-                s.add(r[0]);
+    function icuLinkify(clazz) {
+        const ces = document.getElementsByClassName(clazz);
+        for(let ce of ces) {
+            const linksBox = document.createElement('div');
+
+            // now, what to add?
+            const s = new Set(); // set of issue links
+
+            for(let subnode of ce.childNodes) {
+                const linkmatch=/ICU-[0-9]+/g;
+                let r;
+                while ((r = linkmatch.exec(subnode.textContent)) !== null) {
+                    s.add(r[0]);
+                }
             }
-        }
 
-        if(s.size) {
-            linksBox.appendChild(document.createTextNode('JIRA: '));
-            ce.appendChild(linksBox);
-        
-            for(let v of s.values()) {
-                const a = document.createElement('a');
-                a.appendChild(document.createTextNode(v));
-                a.href=`https://unicode-org.atlassian.net/browse/${v}`;
-                linksBox.appendChild(a);
-                linksBox.appendChild(document.createTextNode(' '));
+            if(s.size) {
+                // linksBox.appendChild(document.createTextNode('JIRA: '));
+                ce.appendChild(linksBox);
+            
+                for(let v of s.values()) {
+                    const a = document.createElement('a');
+                    a.className = 'btn btn-sm';
+                    a.appendChild(document.createTextNode(v));
+                    a.href=`${jirasite}/browse/${v}`;
+                    updateStatus(v, a);
+                    linksBox.appendChild(a);
+                    linksBox.appendChild(document.createTextNode(' '));
+                }
             }
         }
     }
-}
-icuLinkify('commit');
-icuLinkify('gh-header-title');
-icuLinkify('js-issue-row');
+    icuLinkify('commit');
+    icuLinkify('gh-header-title');
+    icuLinkify('js-issue-row');
+})();
+
